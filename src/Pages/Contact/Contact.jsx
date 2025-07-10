dfe290f58023b01df5ad65614ec3cdf80d6deaa4
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-07-10 21:48:53 +0600

    updating..

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -57,7 +57,7 @@ export default function Contact() {
             </div>
             <div className="flex items-center space-x-3">
               <FaPhoneAlt className="text-green-600 w-5 h-5" />
-              <span>+8801560023160</span>
+              <span>+8801621741799</span>
             </div>
             <div className="flex items-center space-x-3">
               <FaMapMarkerAlt className="text-green-600 w-5 h-5" />
